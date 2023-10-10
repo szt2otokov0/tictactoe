aab48378dedb9deaa0c29a60345dcdb8facb55f8
szt2otokov <szt2otokov@vasvari.org>
2023-10-10 12:52:28 +0200

    rewrite but its not safe to index matrices :(

--- js.js
+++ js.js
@@ -1,41 +1,45 @@
 const PlayerChar = "X";
 const CpuChar = "O";
+let spaceMatrix = [[], [], []];
 
 window.onload = () => {
     let columns = document.querySelectorAll(".column");
-    let spaceMatrix = [[], [], []];
+    spaceMatrix = [[], [], []];
     for (i = 0; i < columns.length; i++) {
         spaceMatrix[i] = columns[i].children;
+        for (space of spaceMatrix[i]) {
+            space.addEventListener("click", (ev) => mark(ev.target))
+        }
     }
-    let spaces = document.querySelectorAll(".space");
-    spaces.forEach(s => {
-        s.addEventListener("click", () => {
-            mark(PlayerChar, s)
-            winCheck();
-            cpuRound();
-        })
-    })
-    function cpuRound() {
-        spaces.forEach(s => {
-            try {
-                mark(CpuChar, s)
-            }
-            catch (state) {
-                if (state !== false) {
-                    mark(CpuChar, s)
-                }
-            }
-        })
+    console.log(spaceMatrix)
+}
 
-        function winCheck() {
-            //todo: do the logic
+function mark(space) {
+    if (space.innerHTML != "") return;
+    space.innerHTML = PlayerChar;
+    let maxDistance = 0;
+    for (i = 0; i < spaceMatrix.length; i++) {
+        for (j = 0; j < spaceMatrix[i].length; j++) {
+            if (spaceMatrix[i][j].innerHTML == "") {
+                continue;
+            }
+            if (checkPlayer(spaceMatrix[i][j + 1]) || checkPlayer(spaceMatrix[i][j - 1]) ||
+                checkPlayer(spaceMatrix[i + 1][j]) || checkPlayer(spaceMatrix[i - 1][j]) ||
+                checkPlayer(spaceMatrix[i + 1][j + 1]) || checkPlayer(spaceMatrix[i + 1][j - 1]) ||
+                checkPlayer(spaceMatrix[i - 1][j + 1]) || checkPlayer(spaceMatrix[i - 1][j - 1])) {
+                maxDistance++;
+            }
+            console.log("checking.., max distance so far:" + maxDistance)
+            if (maxDistance === 3) {
+                alert("Nyertél!")
+            }
         }
     }
 
-    
-    function mark(sign, s) {
-        if (s.innerHTML != "") throw false;
-        s.innerHTML = sign;
+    function checkPlayer(spaceToCheck) {
+        if (spaceToCheck === undefined) return false;
+        spaceToCheck.style = "background-color: green"
+        return (spaceToCheck.innerHTML == PlayerChar);
     }
 }
 
